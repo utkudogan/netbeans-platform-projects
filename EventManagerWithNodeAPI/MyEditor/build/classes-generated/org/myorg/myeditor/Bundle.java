9073cf57cab880724f25e0e67e6978e690ba3609
utkudogan <52001660+utkudogan@users.noreply.github.com>
2024-05-13 11:01:51 +0300

    add Node API

---
+++ EventManagerWithNodeAPI/MyEditor/build/classes-generated/org/myorg/myeditor/Bundle.java
@@ -0,0 +1,12 @@
+package org.myorg.myeditor;
+/** Localizable strings for {@link org.myorg.myeditor}. */
+class Bundle {
+    /**
+     * @return <i>Open Editor</i>
+     * @see MyEditor
+     */
+    static String CTL_MyEditorBeanTreeViewAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_MyEditorBeanTreeViewAction");
+    }
+    private Bundle() {}
+}
